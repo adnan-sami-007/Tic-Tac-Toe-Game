f5fa9ff9277d7591fee12883b97dad13d371a0ce
Adnan Sami <adnansami@qq.com>
2025-08-22 01:50:33 +0800

    added js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const boxes = document.querySelectorAll(".box");
 
+const winningPatten = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 let turnO = true;
 
 for (let box of boxes) {
