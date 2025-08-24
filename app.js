46cbea5271b72b5639d2b73cda050c9b5d09ed4f
Adnan Sami <adnansami@qq.com>
2025-08-24 16:21:36 +0800

    add js logic

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const winningPatten = [
   [2, 4, 6],
 ]
 
+let userO = 0;
+let userX = 0;
+
 let turnO = true;
 
 for (let box of boxes) {
@@ -24,5 +27,20 @@ for (let box of boxes) {
     }
     box.disabled = true;
     console.log("clicked");
+    checkWinner();
   })
+}
+
+const checkWinner = () => {
+  for(let patten of winningPatten) {
+    let pos1Val = boxes[patten[0]].innerText;
+    let pos2Val = boxes[patten[1]].innerText;
+    let pos3Val = boxes[patten[2]].innerText;
+
+    if(pos1Val !== '' && pos2Val !== '' && pos3Val !== '') {
+      if(pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("winner", pos1Val);
+      }
+    }
+  }
 }
